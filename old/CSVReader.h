21e224db0864cc9f440a5b5b0b2b3bdee5bd84d5
Egorz734 <egorz734@mail.ru>
2020-05-17 23:58:14 +0300

    Добавил 2 и 3 лабы с подправленным кодом
    + CSVReader.h - по задумке нужно будет его менять, причем сильно
    + main.cpp - для того, чтобы принцип исопльзования классов из RKmethod.h был более наглядным

---
+++ old/CSVReader.h
@@ -0,0 +1,116 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <windows.h>
+#include <Shellapi.h>
+#include "linear_alg.h"
+
+
+class CSVRow
+{
+public:
+	std::string const& operator[](std::size_t index) const
+	{
+		return m_data[index];
+	}
+	std::size_t size() const
+	{
+		return m_data.size();
+	}
+	void readNextRow(std::istream& str)
+	{
+		std::string line;
+		std::getline(str, line);
+
+		std::stringstream lineStream(line);
+		std::string cell;
+
+		m_data.clear();
+		while (std::getline(lineStream, cell, ','))
+		{
+			m_data.push_back(cell);
+		}
+		if (!lineStream && cell.empty())
+		{
+			m_data.push_back("");
+		}
+	}
+	friend std::ostream& operator<<(std::ostream& out, CSVRow& f)// to run tests of CSV lib
+	{
+		for (size_t i = 0; i < f.size(); i++)
+			out << f.m_data[i] << " ";
+		return out;
+	}
+	template<typename T>
+	friend Matrix<T> CSVtoMatrix(std::string file_name);
+
+private:
+	std::vector<std::string> m_data;
+};
+
+std::istream& operator>>(std::istream& str, CSVRow& data)
+{
+	data.readNextRow(str);
+	return str;
+}
+
+template<typename T>
+Matrix<T> CSVtoMatrix(std::string file_name)
+{
+	std::ifstream file(file_name);
+	std::vector<CSVRow> data;
+	CSVRow row;
+
+	while (file >> row)
+	{
+		data.push_back(row);
+	}
+	size_t n_lines = data.size();
+	size_t n_columns = data[0].size();
+	Matrix<T> tmp(n_lines, n_columns);
+	for (size_t i = 0; i < n_lines; i++)
+	{
+		for (size_t j = 0; j < n_columns; j++)
+		{
+			tmp[i][j] = std::stod(data[i][j]);
+		}
+	}
+
+	return tmp;
+}
+
+template <typename T>
+void MatrixtoCSV(std::string file_name, Matrix<T> mat)
+{
+	std::ofstream myfile;
+	myfile.open(file_name);
+	size_t strok = mat.get_amount_of_lines();
+	size_t stolb = mat.get_amount_of_columns();
+	for (size_t i = 0; i < strok; i++)
+	{
+		for (size_t j = 0; j < stolb; j++)
+		{
+			myfile << std::to_string(mat[i][j]) << ",";
+		}
+		myfile << "\n";
+	}
+}
+
+
+template <typename T>
+void write_in_file(std::string file_name, std::vector<Matrix<T>> res)
+{
+	std::ofstream myfile;
+	myfile.open(file_name);
+	for (size_t i = 0; i < res.size(); i++)
+	{
+		for(size_t j = 0; j < res[i].get_amount_of_lines() - 1;j++)
+			myfile << std::to_string(res[i][j][0]) << ",";
+		myfile << std::to_string(res[i][res[i].get_amount_of_lines() - 1][0]) << "\n";
+	}
+	myfile.close();
+	std::cout << "Successfully written in " << file_name << std::endl;
+}
