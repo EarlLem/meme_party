e71c63d6e09addff6f59639c97f2217f5ab6c530
Egorz734 <egorz734@mail.ru>
2020-05-19 01:41:53 +0300

    проверка гит игнор

--- old/laba1_2/main.cpp
+++ old/laba1_2/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
 	Matrix<double> state(2,1);
 	state[0][0] = -1.;
-	state[1][0] = 0.;
+	state[1][0] = 0;
 	RKintegrator<RKmethod_5_4_7fs, Osc_Van_der_Pol> integ1;
 //	RKintegrator<VernerRK_5_6_8, Osc_Van_der_Pol> integ2;
 	std::vector<Matrix<double>> res1 = integ1.n_steps(0, state, 0.01, 50);
