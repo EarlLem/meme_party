c75310d4ee9d350006956bd8646d1f076448bd7b
EarlLem <lkdesyatkina@gmail.com>
2020-05-26 11:25:43 +0300

    Update RKmethod.h
    + 2 RKmethods

--- Group_project/Group_project/RKmethod.h
+++ Group_project/Group_project/RKmethod.h
@@ -302,6 +302,251 @@ private:
 	Matrix<double> b_subs;
 };
 
+class RK57FEq1 // (5,2)
+{
+public:
+	RK57FEq1()
+	{
+		A = Matrix<double>(7, 7);
+		for (size_t i = 0; i < 7; i++)
+			A[0][i] = 0.;
+
+		A[1][0] = 2. / 9;
+		for (size_t i = 1; i < 7; i++)
+			A[1][i] = 0.;
+
+		A[2][0] = 1. / 12;
+		A[2][1] = 1. / 4;
+		for (size_t i = 2; i < 7; i++)
+			A[2][i] = 0.;
+
+		A[3][0] = 1. / 8;
+		A[3][1] = 0.;
+		A[3][2] = 3. / 8;
+		for (size_t i = 3; i < 7; i++)
+			A[3][i] = 0.;
+
+		A[4][0] = 91. / 500;
+		A[4][1] = -27. / 100;
+		A[4][2] = 78. / 125;
+		A[4][3] = 8. / 125;
+		for (size_t i = 4; i < 7; i++)
+			A[4][i] = 0.;
+
+		A[5][0] = -11. / 20;
+		A[5][1] = 27. / 20;
+		A[5][2] = 12. / 5;
+		A[5][3] = -36. / 5;
+		A[5][4] = 5.;
+		A[5][5] = 0.;
+		A[5][6] = 0.;
+
+		A[6][0] = 1. / 12;
+		A[6][1] = 0.;
+		A[6][2] = 27. / 32;
+		A[6][3] = -4. / 3;
+		A[6][4] = 125. / 96;
+		A[6][5] = 5. / 48;
+		A[6][6] = 0.;
+
+		c = Matrix<double>(7, 1);
+		c[0][0] = 0.;
+		c[1][0] = 2. / 9;
+		c[2][0] = 1. / 3;
+		c[3][0] = 1. / 2;
+		c[4][0] = 3. / 5;
+		c[5][0] = 1.;
+		c[6][0] = 1.;
+
+		b_main = Matrix<double>(7, 1);
+		b_main[0][0] = 1. / 12;
+		b_main[1][0] = 0.;
+		b_main[2][0] = 27. / 32;
+		b_main[3][0] = -4. / 3;
+		b_main[4][0] = 125. / 96;
+		b_main[5][0] = 5. / 48;
+		b_main[6][0] = 0.;
+
+		b_subs = Matrix<double>(7, 1);
+		b_subs[0][0] = 2. / 15;
+		b_subs[1][0] = 0.;
+		b_subs[2][0] = 27. / 80;
+		b_subs[3][0] = -2. / 15;
+		b_subs[4][0] = 25. / 48;
+		b_subs[5][0] = 1. / 24;
+		b_subs[6][0] = 1. / 10;
+
+		/*
+				orders=[5;4];
+				isFSAL=true;
+		*/
+	}
+	Matrix<double> execute(double t_0, Matrix<double>& y_0, double h, Matrix<double>(*f)(double, Matrix<double>))
+	{
+		std::vector<Matrix<double>> k;
+		Matrix<double> tmp_y(y_0.get_amount_of_lines(), 1);
+		k.push_back(f(t_0, y_0));
+		for (size_t i = 1; i < 7; i++)
+		{
+			tmp_y = tmp_y * 0;
+			for (size_t j = 0; j < i; j++)
+			{
+				tmp_y = tmp_y + A[i][j] * k[j];
+			}
+			k.push_back(f(t_0 + c[i][0] * h, y_0 + tmp_y * h));
+		}
+		Matrix<double> tmp_acc(y_0.get_amount_of_lines(), 1, 0.);
+		tmp_y = y_0;
+		tmp_acc = y_0;
+		for (size_t i = 0; i < 7; i++)
+		{
+			tmp_y = tmp_y + h * b_main[i][0] * k[i];
+			tmp_acc = tmp_acc + h * b_subs[i][0] * k[i];
+		}
+
+		Matrix<double> ttmp_y(tmp_y.get_amount_of_lines() + 1, 1);
+		for (size_t i = 0; i < tmp_y.get_amount_of_lines(); i++)
+			ttmp_y[i][0] = tmp_y[i][0];
+		ttmp_y[tmp_y.get_amount_of_lines()][0] = (tmp_acc - tmp_y).mod();
+		return ttmp_y;
+	}
+private:
+	Matrix<double> A;
+	Matrix<double> c;
+	Matrix<double> b_main;
+	Matrix<double> b_subs;
+};
+
+class RK68M // (6,2)
+{
+public:
+	RK68M()
+	{
+		A = Matrix<double>(8, 8);
+		for (size_t i = 0; i < 8; i++)
+			A[0][i] = 0.;
+
+		A[1][0] = 1. / 10;
+		for (size_t i = 1; i < 8; i++)
+			A[1][i] = 0.;
+
+		A[2][0] = -2. / 81;
+		A[2][1] = 20. / 91;
+		for (size_t i = 2; i < 8; i++)
+			A[2][i] = 0.;
+
+		A[3][0] = 615. / 1372;
+		A[3][1] = -270./343;
+		A[3][2] = 1053. / 1372;
+		for (size_t i = 3; i < 8; i++)
+			A[3][i] = 0.;
+
+		A[4][0] = 3243. / 5500;
+		A[4][1] = -54. / 55;
+		A[4][2] = 50949. / 71500;
+		A[4][3] = 4998. / 17875;
+		for (size_t i = 4; i < 8; i++)
+			A[4][i] = 0.;
+
+		A[5][0] = -26492. / 37125;
+		A[5][1] = 72. / 55;
+		A[5][2] = 2808. / 23375;
+		A[5][3] = -24206. / 37125;
+		A[5][4] = 338./459;
+		A[5][5] = 0.;
+		A[5][6] = 0.;
+		A[5][7] = 0.;
+
+		A[6][0] = 5561. / 2376;
+		A[6][1] = -35./11;
+		A[6][2] = -24117. / 31603;
+		A[6][3] = 899983. / 200772;
+		A[6][4] = -5225. / 1836;
+		A[6][5] = 3925. / 4056;
+		A[6][6] = 0.;
+		A[6][7] = 0.;
+
+		A[7][0] = 465467. / 266112;
+		A[7][1] = -2945. / 1232;
+		A[7][2] = -5610201. / 14158144;
+		A[7][3] = 10513573. / 2312352;
+		A[7][4] = -424325. / 205632;
+		A[7][5] = 376225. / 454272;
+		A[7][6] = 0.;
+		A[7][6] = 0.;
+
+		c = Matrix<double>(7, 1);
+		c[0][0] = 0.;
+		c[1][0] = 1. / 10;
+		c[2][0] = 2. / 9;
+		c[3][0] = 3. / 7;
+		c[4][0] = 3. / 5;
+		c[5][0] = 4./5;
+		c[6][0] = 1.;
+		c[7][0] = 1.;
+
+
+		b_main = Matrix<double>(7, 1);
+		b_main[0][0] = 61. / 864;
+		b_main[1][0] = 0.;
+		b_main[2][0] = 98415. / 321776;
+		b_main[3][0] = 16807. / 146016;
+		b_main[4][0] = 1375. / 7344;
+		b_main[5][0] = 1375. / 5408;
+		b_main[6][0] = -37./1120;
+		b_main[7][0] = 1. / 10;
+
+		b_subs = Matrix<double>(7, 1);
+		b_subs[0][0] = 821. / 10800;
+		b_subs[1][0] = 0.;
+		b_subs[2][0] = 19683. / 71825;
+		b_subs[3][0] = 175273. / 912600;
+		b_subs[4][0] = 395. / 3672;
+		b_subs[5][0] = 758. / 2704;
+		b_subs[6][0] = 3. / 50;
+		b_subs[7][0] = 0.;
+
+		/*
+				orders=[6;5];
+				isFSAL=false;
+		*/
+	}
+	Matrix<double> execute(double t_0, Matrix<double>& y_0, double h, Matrix<double>(*f)(double, Matrix<double>))
+	{
+		std::vector<Matrix<double>> k;
+		Matrix<double> tmp_y(y_0.get_amount_of_lines(), 1);
+		k.push_back(f(t_0, y_0));
+		for (size_t i = 1; i < 8; i++)
+		{
+			tmp_y = tmp_y * 0;
+			for (size_t j = 0; j < i; j++)
+			{
+				tmp_y = tmp_y + A[i][j] * k[j];
+			}
+			k.push_back(f(t_0 + c[i][0] * h, y_0 + tmp_y * h));
+		}
+		Matrix<double> tmp_acc(y_0.get_amount_of_lines(), 1, 0.);
+		tmp_y = y_0;
+		tmp_acc = y_0;
+		for (size_t i = 0; i < 8; i++)
+		{
+			tmp_y = tmp_y + h * b_main[i][0] * k[i];
+			tmp_acc = tmp_acc + h * b_subs[i][0] * k[i];
+		}
+
+		Matrix<double> ttmp_y(tmp_y.get_amount_of_lines() + 1, 1);
+		for (size_t i = 0; i < tmp_y.get_amount_of_lines(); i++)
+			ttmp_y[i][0] = tmp_y[i][0];
+		ttmp_y[tmp_y.get_amount_of_lines()][0] = (tmp_acc - tmp_y).mod();
+		return ttmp_y;
+	}
+private:
+	Matrix<double> A;
+	Matrix<double> c;
+	Matrix<double> b_main;
+	Matrix<double> b_subs;
+};
+
 template<typename RK, typename LV>
 class RKintegrator : public algorithm
 {
